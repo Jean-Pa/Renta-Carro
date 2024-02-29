7289c0c081772af14324f3f17d9daea5f6cbd848
Jean-pa <jcarlo_p_p@hotmail.com>
2024-02-28 23:09:13 -0500

    Añadiendo el repositorio

---
+++ renta_carro/src/main/java/com/jean/car/RentaCarroApplication.java
@@ -0,0 +1,13 @@
+package com.jean.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaCarroApplication.class, args);
+	}
+
+}
